0e045e3e2771b3b6ca817f619abf714f5b30e1ae
02Andre <02andre.smith@gulllakecs.org>
2020-03-05 11:13:52 -0500

    main program file

--- Serina.py
+++ Serina.py
@@ -2,14 +2,17 @@
 from gtts import gTTS
 import speech_recognition as sr
 import os
+import re
 import webbrowser
 import smtplib
+import requests
+from weather import Weather
 
 def talkToMe(audio):
+    "speaks audio as argument"
     print(audio)
-    tts = gTTS(text=audio, lang='eng')
-    tts.save('audio.mp3')
-    os.system('mpg123 audio.mp3')
+          for line in audio.splitlines():
+    os.system("say" + audio)
 
 #listens to commands
 def myCommand():
@@ -20,11 +23,12 @@ def myCommand():
         r.adjust_for_ambient_noise(source, duration = 1)
         audio = r.listen(source)
     try:
-        command = r.recongnize_google(audio)
+        command = r.recongnize_google(audio).lower()
         print('You said: ' + command + '\n')
     #loop back to continue to listen to commands
     except sr.UnknownValueError:
-        assistant(myCommand())
+        print('I couldn\'t hear your last command')
+        command = myCommand();
     return command
 
 #if statements for execution commands
@@ -33,28 +37,58 @@ def assistant(command):
         chrome_path = '/usr/bin/google-chrome'
         url = 'https://www.https://www.google.com/?safe=active&ssui=on'
         webbrowser.get(chrome_path).open(url)
-    if 'what\'s up' or 'how are you' in command:
+    elif 'what\'s up' or 'how are you' in command:
         talkToMe('I\'m doing Lovely')
-    if 'email' in command:
-        talkToMe('who is the recipient')
+    elif "current weather in" in command:
+        reg_ex = re.search('current waether in (.*)', command)
+        if reg_ex:
+            city = reg_ex.group(1)
+            weather = Weather()
+            location = weather.lookup_by_location(city)
+            condition = location.condition()
+            talkToMe('The Current weather in %s is %s The tempeture is %.1f degree' % (city, condition.text(), (int(condition.temp()) - 32) / 1.8))
+    elif 'weather forecast in' in command:
+        reg_ex = re.search('weather forecast in (.*)', command)
+        if reg_ex:
+            city = reg_ex.group(1)
+            weather = Weather()
+            location = weather.lookup_by_location(city)
+            forecasts = location.forecast()
+            for i in range(0, 3):
+                talkToMe('On %s will it %s. The maximum temperture will be %.1f degree.'
+                         'The lowest temperature will be %.1f degrees.' % (forecasts[i].date(), forecasts[i].text(), (int(forecasts[i].high()) - 32) / 1.8,(int(forecasts[i].low()) - 32) / 1.8))
+    elif 'email' in command:
+        talkToMe('Who is the recipient?')
         recipient = myCommand()
-        if 'Lange' or ' Brad' in recipient:
-            talkToMe('what should I say?' )
+
+        if 'Andre' in recipient:
+            talkToMe('What should I say?')
             content = myCommand()
 
-            #init gmail SMTP
-            mail = smtplid.SMTP('smtp.gulllakecs.org', 587)
-            #identify to server
+            # init gmail SMTP
+            mail = smtplib.SMTP('smtp.gulllakecs.org', 209)
+
+            # identify to server
             mail.ehlo()
-            #Encrypt session
+
+            # encrypt session
             mail.starttls()
-            #login
-            mail.login( 'user', 'pass')
-            #send mail
-            mail.sendmail('Lange', 'email ', content )
-            #close connection
+
+            # login
+            mail.login('02andre.smith@gulllakecs.org', 'Clam61acre')
+
+            # send message
+            mail.sendmail('Andre Smith', '15andresmith@gmail.com', content)
+
+            # end mail connection
             mail.close()
-            talkToMe('Email has been sent')
+
+            talkToMe('Email sent.')
+
+        else:
+            talkToMe('I don\'t know what you mean!')
+
+
 talkToMe('anything else sir')
 while True:
     assistant(myCommand())
